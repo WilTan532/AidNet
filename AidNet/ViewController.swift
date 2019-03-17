1ddc26c2b739df492294c04a1a1c4d5744f78a4a
shuyu liu <shuyuliu@shuyus-macbook-pro.local>
2019-03-17 00:03:40 -0400

    Initial Commit

---
+++ AidNet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AidNet
+//
+//  Created by shuyu liu on 2019-03-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
